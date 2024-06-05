c8a76a1c5b5f819f53db83907329a6f35a5cab7f
Bob <SBob.com>
2024-06-05 10:04:38 +0800

    first commit

---
+++ do.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+git add 02、Endnote\ 教程（罗昭锋）/Endnote\ X5/
+git commit -m "first commit"
+git push -u endnote main
+git add 02、Endnote\ 教程（罗昭锋）/
+git commit -m "first commit"                                                                 
+git push -u endnote main
+git add  06、EndNote\ X8\ 教程（独家）/旧版本/视频教程/20120329*
+git commit -m "first commit"                   
+git push -u endnote main
+git add  06、EndNote\ X8\ 教程（独家）/旧版本/视频教程/
+git commit -m "first commit"                                                                                                              
+git push -u endnote main
+git add  06、EndNote\ X8\ 教程（独家）/
+git commit -m "first commit"       
+git push -u endnote main
+git add 10、Endnote文本教程/EndNote*
+git commit -m "first commit" 
+git push -u endnote main
+git add 10、Endnote文本教程/endnote*
+git commit -m "first commit"    
+git push -u endnote main
+git add 10、Endnote文本教程/*
+git commit -m "first commit"                                                                                                                
+git push -u endnote main
+git add 12、【软件安装包】EndNote\ X9\ 教程（中科大）/EndNote\ X9大客户版win+mac（终身正版免费）/
+git commit -m "first commit"
+git push -u endnote main
+git add 12、【软件安装包】EndNote\ X9\ 教程（中科大）/
+git commit -m "first commit"
+git push -u endnote main
+git add  13、其他资料/
+git commit -m "first commit"
+git push -u endnote main
+git add .
+git commit -m "first commit"
+git push -u endnote main
+
+
+
